bfa087239a491db78378ae8ce8b7bcb1010752ae
hsu772 <hy772@live.com>
2016-04-21 20:10:32 +0800

    Add the common at MainActivity.java
    Class: 2016.04/21

--- SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
@@ -7,19 +7,20 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView textView; //variable name
+    TextView textView2; //variable name,
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = (TextView)findViewById(R.id.textView);
+        textView2 = (TextView)findViewById(R.id.textView); //The "textView" of "R.id.textView" map to the id "textView" (at activity_main.xml)
+                                                          // The code try to get the id name "textView"
 
     }
 
-    public void click(View view)
+    public void click(View view) //The name "click" will map to Properties: onClick  (at activity_main.xml)
     {
-        textView.setText("Android Class2");
+        textView2.setText("Android Class2"); // When click the button, it will change the text "Android Class2" to the id:textView
     }
 }
